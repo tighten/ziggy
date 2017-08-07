540a90393200adbd997b409daadb477bd8ba72a7
Daniel Coulbourne <daniel@tighten.co>
2017-08-07 13:45:55 -0400

    Circumvent view:cache on local and add config to do it on other environments; document this.

--- src/ZiggyServiceProvider.php
+++ src/ZiggyServiceProvider.php
@@ -4,11 +4,21 @@ namespace Tightenco\Ziggy;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Tightenco\Ziggy\NoCacheBladeCompiler;
+use Tightenco\Ziggy\UnCachedViewFinder;
 
 class ZiggyServiceProvider extends ServiceProvider
 {
     public function boot(BladeRouteGenerator $generator)
     {
+        if (config('app.env') === 'local' || config('app.ziggy_cache') === false) {
+            $this->app->singleton('blade.compiler', function () {
+                return new NoCacheBladeCompiler(
+                    $this->app['files'], $this->app['config']['view.compiled']
+                );
+            });
+        }
+
         Blade::directive('routes', function () use ($generator) {
             return $generator->generate();
         });
