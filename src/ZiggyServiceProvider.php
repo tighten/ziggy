64b1a1746fb9a74f5ee993ae87a906abe53252ec
Muah <muah003@gmail.com>
2017-08-11 09:34:18 +0200

    init

--- src/ZiggyServiceProvider.php
+++ src/ZiggyServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Tightenco\Ziggy;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,5 +13,23 @@ class ZiggyServiceProvider extends ServiceProvider
         Blade::directive('routes', function () {
             return "<?php echo app('" . BladeRouteGenerator::class . "')->generate(); ?>";
         });
+
+        /**
+         * generate file automatically.
+         *
+         * todo: add blade directive or whatever to
+         * get the file dis-regarding of the time stamp
+         */
+        $routes_folder     = base_path('routes');
+        $modification_time = filemtime($routes_folder);
+        $js_data           = File::get(__DIR__.'/js/route.js');
+
+        $new_path = public_path("assets/js/ziggy.{$modification_time}.js");
+        $old_path = public_path('assets/js/ziggy.*.js');
+
+        if (!File::exists($new_path)) {
+            File::delete(File::glob($old_path));
+            File::put($new_path, $js_data);
+        }
     }
 }
