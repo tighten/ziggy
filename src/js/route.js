cab68ae620e449e49385a6125d233129d7d325cf
Jesse Leite <jesse.leite@roomroster.com>
2017-08-11 14:17:43 -0400

    Add support for unkeyed params array.

--- src/js/route.js
+++ src/js/route.js
@@ -1,14 +1,16 @@
 var route = function(name, params = {}, absolute = true) {
     var domain = (namedRoutes[name].domain || baseUrl).replace(/\/+$/,'') + '/',
-        url = (absolute ? domain : '') + namedRoutes[name].uri
+        url = (absolute ? domain : '') + namedRoutes[name].uri,
+        arrayKey = 0;
 
     return url.replace(
         /\{([^}]+)\}/gi,
         function (tag) {
-            var key = tag.replace(/\{|\}/gi, '');
+            var key = Array.isArray(params) ? arrayKey : tag.replace(/\{|\}/gi, '');
             if (params[key] === undefined) {
                 throw 'Ziggy Error: "' + key + '" key is required for route "' + name + '"';
             }
+            arrayKey++;
             return params[key];
         }
     );
