4bc184e82c7f706e41f769f465822d6627a64907
enssnert <thomas.enssner@herzotec.de>
2017-10-09 16:05:33 +0200

    fix wrong usage of constants

--- src/BladeRouteGenerator.php
+++ src/BladeRouteGenerator.php
@@ -49,9 +49,10 @@ EOT;
     {
         $url = url('/');
         $parsedUrl = parse_url($url);
+
         $this->baseUrl = $url . '/';
-        $this->baseProtocol = array_key_exists(PHP_URL_SCHEME, $parsedUrl) ? $parsedUrl[PHP_URL_SCHEME] : 'http';
-        $this->baseDomain = array_key_exists(PHP_URL_HOST, $parsedUrl) ? $parsedUrl[PHP_URL_HOST] : '';
-        $this->basePort = array_key_exists(PHP_URL_PORT, $parsedUrl) ? $parsedUrl[PHP_URL_PORT] : 'false';
+        $this->baseProtocol = array_key_exists('scheme', $parsedUrl) ? $parsedUrl['scheme'] : 'http';
+        $this->baseDomain = array_key_exists('host', $parsedUrl) ? $parsedUrl['host'] : '';
+        $this->basePort = array_key_exists('port', $parsedUrl) ? $parsedUrl['port'] : 'false';
     }
 }
