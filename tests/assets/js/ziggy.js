9e0617183015d466d3058e84bcbd582d70a20f8f
Ankur Kumar <ankurk91@users.noreply.github.com>
2017-11-25 12:06:06 +0530

    Fix failing test
    This test was failing since we removed JSON.parse()

--- tests/assets/js/ziggy.js
+++ tests/assets/js/ziggy.js
@@ -1,5 +1,5 @@
     var Ziggy = {
-        namedRoutes: JSON.parse('{"postComments.index":{"uri":"posts\/{post}\/comments","methods":["GET","HEAD"],"domain":null}}'),
+        namedRoutes: {"postComments.index":{"uri":"posts\/{post}\/comments","methods":["GET","HEAD"],"domain":null}},
         baseUrl: 'http://localhostmyapp.com/',
         baseProtocol: 'http',
         baseDomain: 'localhostmyapp.com',
