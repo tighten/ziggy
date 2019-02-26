53c5e162f527fce9759a5bff9594f38af336675e
Steve Widerberg <steve.widerberg@barchart.com>
2019-02-26 14:38:29 -0600

    Fix current method with optional parameters

--- tests/js/test.route.js
+++ tests/js/test.route.js
@@ -75,6 +75,11 @@ global.Ziggy = {
             "uri": "optional/{id}/{slug?}",
             "methods": ["GET","HEAD"],
             "domain": null
+        },
+        "another-optional": {
+            "uri": "optional/{slug?}",
+            "methods": ["GET","HEAD"],
+            "domain": null
         }
     },
     baseUrl: 'http://myapp.dev/',
@@ -538,6 +543,62 @@ describe('route()', function() {
         assert.equal(
             false,
             route().current("*.users.show")
+        );
+
+        global.Ziggy.baseUrl = orgBaseUrl;
+        global.Ziggy.baseDomain = orgBaseDomain;
+        global.Ziggy.basePort = orgBasePort;
+    });
+
+    it('Should return correct route name for current() when a route has does not have optional parameters.', function() {
+        let orgBaseUrl = Ziggy.baseUrl;
+        let orgBaseDomain = Ziggy.baseDomain;
+        let orgBasePort = Ziggy.basePort;
+
+        global.Ziggy.baseUrl = 'http://myapp.dev:81/';
+        global.Ziggy.baseDomain = 'myapp.dev';
+        global.Ziggy.basePort = 81;
+
+        global.window = {
+            location: {
+                hostname: "myapp.dev",
+                pathname: "/optional/1",
+                port: "81",
+                protocol: "http:"
+            }
+        };
+
+        assert.equal(
+            true,
+            route().current('optional')
+        );
+
+        global.Ziggy.baseUrl = orgBaseUrl;
+        global.Ziggy.baseDomain = orgBaseDomain;
+        global.Ziggy.basePort = orgBasePort;
+    });
+
+    it('Should return correct route name for current() when a route has optional parameters.', function() {
+        let orgBaseUrl = Ziggy.baseUrl;
+        let orgBaseDomain = Ziggy.baseDomain;
+        let orgBasePort = Ziggy.basePort;
+
+        global.Ziggy.baseUrl = 'http://myapp.dev:81/';
+        global.Ziggy.baseDomain = 'myapp.dev';
+        global.Ziggy.basePort = 81;
+
+        global.window = {
+            location: {
+                hostname: "myapp.dev",
+                pathname: "/optional/1/2",
+                port: "81",
+                protocol: "http:"
+            }
+        };
+
+        assert.equal(
+            true,
+            route().current('optional')
         );
 
         global.Ziggy.baseUrl = orgBaseUrl;
