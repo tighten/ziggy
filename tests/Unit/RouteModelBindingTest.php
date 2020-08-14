ef734eeb5bf447de6d37eb59f80c566105e4e6e9
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2020-08-14 17:15:44 -0400

    Add and clean up tests

--- tests/Unit/RouteModelBindingTest.php
+++ tests/Unit/RouteModelBindingTest.php
@@ -3,10 +3,8 @@
 namespace Tests\Unit;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
-use Tightenco\Ziggy\Ziggy;
 use Tests\TestCase;
-use Tightenco\Ziggy\BladeRouteGenerator;
+use Tightenco\Ziggy\Ziggy;
 
 class RouteModelBindingTest extends TestCase
 {
@@ -19,21 +17,76 @@ class RouteModelBindingTest extends TestCase
         $router->get('users/{user}', function (User $user) {
             return '';
         })->name('users');
-
-        $router->get('blog/{category}/{post:slug}', function (PostCategory $category, Post $post) {
+        $router->get('tokens/{token}', function ($token) {
             return '';
-        })->name('categories.posts.show');
+        })->name('tokens');
+        $router->get('users/{user}/{number}', function (User $user, int $n) {
+            return '';
+        })->name('users.numbers');
 
-        // if ($this->laravelVersion(7)) {
-        //     $router->get('/posts/{post}/comments/{comment:uuid}', $this->noop())->name('postComments.show');
-        // }
+        if ($this->laravelVersion(7)) {
+            $router->get('blog/{category}/{post:slug}', function (PostCategory $category, Post $post) {
+                return '';
+            })->name('posts');
+        }
 
         $router->getRoutes()->refreshNameLookups();
     }
 
     /** @test */
-    public function can_retrieve_implicit_route_model_bindings()
+    public function can_register_implicit_route_model_bindings()
     {
+        $expected = [
+            'users' => [
+                'uri' => 'users/{user}',
+                'methods' => ['GET', 'HEAD'],
+                'domain' => null,
+                'bindings' => [
+                    'user' => 'uuid',
+                ],
+            ],
+        ];
+
+        $this->assertSame($expected, (new Ziggy)->filter('users')->toArray());
+    }
+
+    /** @test */
+    public function can_ignore_route_parameters_not_bound_to_eloquent_models()
+    {
+        $this->assertSame([
+            'tokens' => [
+                'uri' => 'tokens/{token}',
+                'methods' => ['GET', 'HEAD'],
+                'domain' => null,
+                'bindings' => [],
+            ],
+        ], (new Ziggy)->filter(['tokens'])->toArray());
+    }
+
+    /** @test */
+    public function can_handle_bound_and_unbound_params_in_the_same_route()
+    {
+        $expected = [
+            'users.numbers' => [
+                'uri' => 'users/{user}/{number}',
+                'methods' => ['GET', 'HEAD'],
+                'domain' => null,
+                'bindings' => [
+                    'user' => 'uuid',
+                ],
+            ],
+        ];
+
+        $this->assertSame($expected, (new Ziggy)->filter('users.numbers')->toArray());
+    }
+
+    /** @test */
+    public function can_merge_implicit_and_scoped_bindings()
+    {
+        if (! $this->laravelVersion(7)) {
+            $this->markTestSkipped('Requires Laravel >=7');
+        }
+
         $this->assertSame([
             'users' => [
                 'uri' => 'users/{user}',
@@ -43,7 +96,21 @@ class RouteModelBindingTest extends TestCase
                     'user' => 'uuid',
                 ],
             ],
-            'categories.posts.show' => [
+            'tokens' => [
+                'uri' => 'tokens/{token}',
+                'methods' => ['GET', 'HEAD'],
+                'domain' => null,
+                'bindings' => [],
+            ],
+            'users.numbers' => [
+                'uri' => 'users/{user}/{number}',
+                'methods' => ['GET', 'HEAD'],
+                'domain' => null,
+                'bindings' => [
+                    'user' => 'uuid',
+                ],
+            ],
+            'posts' => [
                 'uri' => 'blog/{category}/{post}',
                 'methods' => ['GET', 'HEAD'],
                 'domain' => null,
