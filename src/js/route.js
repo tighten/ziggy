e8d114120c0a9bc42baab17c0f39387f4eae4fa0
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2020-09-25 11:13:34 -0400

    Clean up formatting and comments, rename _locationURL to _dehydrate

--- src/js/route.js
+++ src/js/route.js
@@ -1,7 +1,5 @@
 import { parse, stringify } from 'qs';
 
-const ts = [/\/$/, ''];
-
 class Route {
     absolute = true;
     bindings = {};
@@ -10,14 +8,19 @@ class Route {
         Object.entries({ name, ...definition, ...config }).map(([key, value]) => this[key] = value);
     }
 
+    // Get a template of the complete URL for this route, e.g. https://{team}.ziggy.dev/user/{user}
     get template() {
+        // If  we're building a relative URL there's no origin, otherwise: if this route has a custom
+        // domain we construct the origin with that, and if not we use the app URL
         const origin = !this.absolute ? '' : this.domain
-            ? `${this.baseProtocol}://${this.domain.replace(...ts)}${this.basePort ? `:${this.basePort}` : ''}`
-            : this.baseUrl.replace(...ts);
+            ? `${this.baseProtocol}://${this.domain.replace(/\/$/, '')}${this.basePort ? `:${this.basePort}` : ''}`
+            : this.baseUrl.replace(/\/$/, '');
 
-        return `${origin}/${this.uri}`.replace(...ts);
+        return `${origin}/${this.uri}`.replace(/\/$/, '');
     }
 
+    // Get an array of objects representing the parameters that this route accepts,
+    // e.g. [{ name: 'team', required: true }, { name: 'user', required: false }]
     get segments() {
         return this.template.match(/{[^}?]+\??}/g)?.map((segment) => ({
             name: segment.replace(/{|\??}/g, ''),
@@ -25,11 +28,14 @@ class Route {
         })) ?? [];
     }
 
+    // Get whether this route's template matches the current window URL
     get current() {
         if (!this.methods.includes('GET')) return false;
 
-        const url = (({ host, pathname }) => `${host}${pathname}`.replace(...ts))(window.location);
+        const url = (({ host, pathname }) => `${host}${pathname}`.replace(/\/$/, ''))(window.location);
 
+        // Transform the route's template into a regex that will match a hydrated URL, by
+        // replacing its parameter segments with matchers for parameter values
         const pattern = this.template
             .replace(/\/{[^}?]*\?}/g, '(\/[^/?]+)?')
             .replace(/{[^}]+}/g, '[^/?]+')
@@ -38,16 +44,17 @@ class Route {
         return new RegExp(`^${pattern}$`).test(url.split('?').shift());
     }
 
+    // Hydrate and return a complete URL for this route with the given parameters
     compile(params) {
         if (!this.segments.length) return this.template;
 
         return this.template.replace(/{([^}?]+)\??}/g, (_, segment) => {
-            if (this.segments.find(({ name }) => name === segment).required && [null, undefined].includes(params[segment])) {
+            if ([null, undefined].includes(params[segment]) && this.segments.find(({ name }) => name === segment).required) {
                 throw new Error(`Ziggy error: '${segment}' parameter is required for route '${this.name}'.`)
             }
 
             return encodeURIComponent(params[segment] ?? '');
-        }).replace(...ts);
+        }).replace(/\/$/, '');
     }
 }
 
@@ -67,8 +74,9 @@ class Router extends String {
         }
     }
 
+    // Parse parameters of any type into a simple object with their keys and values
     _parse(params = {}, route = this._route) {
-        // If 'params' is a string or integer, wrap it in an array
+        // If `params` is a single string or integer, wrap it in an array
         params = ['string', 'number'].includes(typeof params) ? [params] : params;
 
         // Separate segments with and without defaults, and fill in the default values
@@ -78,17 +86,17 @@ class Router extends String {
 
         if (Array.isArray(params)) {
             // If the parameters are an array they have to be in order, so we can transform them into
-            // an object by just keying them with the template segments in the order they appear
+            // an object by keying them with the template segments in the order they appear
             params = params.reduce((result, current, i) => ({ ...result, [segments[i].name]: current }), defaults);
         } else if (segments.length === 1 && !params[segments[0].name] && params[Object.values(route.bindings)[0]]) {
-            // If there is only one template segment and the parameters are an object, that object is
-            // ambiguous—it could be parameter keys and values, or it could just be the one parameter
-            // itself; we can inspect it to find out, and if it's the parameter itself, we wrap it
-            // in an object with its key
+            // If there is only one parameter template segment and `params` is an object, that
+            // object is ambiguous—it could be the parameter key and value, or it could be an
+            // object representing just the parameter value; we can inspect it to find out,
+            // and if it's the parameter value, wrap it in an object with its key
             params = { [segments[0].name]: params };
         }
 
-        // Substitute bindings
+        // Substitute any registered route model bindings for this route
         return Object.entries({ ...defaults, ...params }).reduce((result, [key, value]) => {
             const bound = value && typeof value === 'object' && route.bindings[key];
 
@@ -100,17 +108,19 @@ class Router extends String {
         }, {})
     }
 
-    _locationParams(route) {
+    // Get the parameter values from the current window URL, based on the given route definition
+    _dehydrate(route) {
         let pathname = window.location.pathname
-            .replace(this.config.baseUrl.replace(/^\w*:\/\/[^/]+/, ''), '') // remove subdirectories
+            .replace(this.config.baseUrl.replace(/^\w*:\/\/[^/]+/, ''), '') // Remove subdirectories
             .replace(/^\/+/, '');
 
+        // Given the hydrated string, template, and delimiter, extract and return
+        // an object of parameter names and values from part of a path or URL
         const dehydrate = (hydrated, template = '', delimiter) => {
             const [values, segments] = [hydrated, template].map(s => s.split(delimiter));
 
-            // Return an object of parameter names and values, ignoring
-            // values that don't correspond to a route segment
             return segments.reduce((result, current, i) => {
+                // Ignore values that are empty or don't correspond to a route segment
                 return /^{[^}?]+\??}$/.test(current) && values[i]
                     ? { ...result, [current.replace(/^{|\??}$/g, '')]: values[i] }
                     : result;
@@ -118,12 +128,14 @@ class Router extends String {
         }
 
         return {
-            ...dehydrate(window.location.host, route.domain, '.'), // domain parameters
-            ...dehydrate(pathname, route.uri, '/'), // route parameters
-            ...parse(window.location.search?.replace(/^\?/, '')), // query parameters
+            ...dehydrate(window.location.host, route.domain, '.'), // Domain parameters
+            ...dehydrate(pathname, route.uri, '/'), // Path parameters
+            ...parse(window.location.search?.replace(/^\?/, '')), // Query parameters
         };
     }
 
+    // Get the name of the route matching the current window URL, or, given a route name
+    // and parameters, check if the current window URL and parameters match that route
     current(name, params) {
         // Find the first named route that matches the current URL
         const [current, route] = Object.entries(this.config.namedRoutes).find(
@@ -133,39 +145,46 @@ class Router extends String {
         // If a name wasn't passed, return the name of the current route
         if (!name) return current;
 
-        // Test the passed name against the current route, matching some basic
-        // wildcards so that `events.*` matches `events.show`, etc.
+        // Test the passed name against the current route, matching some
+        // basic wildcards, e.g. `events.*` matches `events.show`
         const match = new RegExp(`^${name.replace('.', '\\.').replace('*', '.*')}$`).test(current);
 
         if (!params) return match;
 
-        // If parameters were passed, check that their values match in the current URL
+        // If parameters were passed, check that their values match in the current window URL
         params = this._parse(params, new Route(current, route, this.config));
 
-        return Object.entries(this._locationParams(route))
+        return Object.entries(this._dehydrate(route))
+            // Only check the parameters that were passed into this method
             .filter(([key]) => params.hasOwnProperty(key))
+            // Use weak equality because all values in the current window URL will be strings
             .every(([key, value]) => params[key] == value);
     }
 
+    // Get the parameter values from the current window URL
     get params() {
-        return this._locationParams(this.config.namedRoutes[this.current()]);
+        return this._dehydrate(this.config.namedRoutes[this.current()]);
     }
 
+    // Check whether the given named route exists
     has(name) {
         return Object.keys(this.config.namedRoutes).includes(name);
     }
 
+    // Add query parameters to be appended to the hydrated URL
     withQuery(params) {
         this.queryParams = { ...this.queryParams, ...params };
         return this;
     }
 
+    // Get the compiled URL for the current route and parameters, as a plain string
     url() {
-        // Get passed parameters that do NOT correspond to route segments
+        // Get passed parameters that do not correspond to any route segments...
         const unhandled = Object.keys(this._params)
             .filter((key) => !this._route.segments.some(({ name }) => name === key))
             .reduce((result, current) => ({ ...result, [current]: this._params[current] }), this.queryParams);
 
+        // ...and append them in the query
         return this._route.compile(this._params) + stringify(unhandled, {
             addQueryPrefix: true,
             arrayFormat: 'indices',
@@ -182,13 +201,13 @@ class Router extends String {
         return this.url();
     }
 
-    // @deprecated Pass parameters as the second argument to `route()`
+    // @deprecated - Pass parameters as the second argument to `route()`
     with(params) {
         this._params = this._parse(params);
         return this;
     }
 
-    // @deprecated Use `has()` instead.
+    // @deprecated - Use `has()` instead
     check(name) {
         return this.has(name);
     }
