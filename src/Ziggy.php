457eb03eaa505ccd9e6627c010f2fe66cbd86d49
Jacob Baker-Kretzmar <bakerkretzmar@users.noreply.github.com>
2021-01-15 10:36:22 -0500

    Use Laravel's Reflector class to get model parameter class name (#396)
    * Use Laravel Reflector to get model parameter type class name
    
    * Fix for older versions of Laravel

--- src/Ziggy.php
+++ src/Ziggy.php
@@ -4,6 +4,7 @@ namespace Tightenco\Ziggy;
 
 use Illuminate\Contracts\Routing\UrlRoutable;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Reflector;
 use Illuminate\Support\Str;
 use JsonSerializable;
 use ReflectionMethod;
@@ -155,7 +156,9 @@ class Ziggy implements JsonSerializable
             $bindings = [];
 
             foreach ($route->signatureParameters(UrlRoutable::class) as $parameter) {
-                $model = $parameter->getType()->getName();
+                $model = class_exists(Reflector::class)
+                    ? Reflector::getParameterClassName($parameter)
+                    : $parameter->getType()->getName();
                 $override = $model === (new ReflectionMethod($model, 'getRouteKeyName'))->class;
 
                 // Avoid booting this model if it doesn't override the default route key name
