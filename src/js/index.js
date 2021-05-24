63ec730556912f22fa6e9e922ee73e73c9674a0c
Jacob Baker-Kretzmar <bakerkretzmar@users.noreply.github.com>
2021-05-24 18:32:00 -0400

    Move Vue plugin into separate file (#424)
    * Export `ZiggyVue` from its own file
    
    * Add test builds
    
    * Update builds and scripts

--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,3 @@ export default function route(name, params, absolute, config) {
 
     return name ? router.toString() : router;
 }
-
-export const ZiggyVue = {
-    install: (v, options) => v.mixin({
-        methods: {
-            route: (name, params, absolute, config = options) => route(name, params, absolute, config),
-        },
-    }),
-};
