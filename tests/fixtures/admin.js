66b4950042e2b8bb525b5c3e058c10cbd5cabf72
Julian Hundeloh <jaulz@users.noreply.github.com>
2021-06-18 03:30:11 +0200

    fix: simplify merge (#429)
    * fix: simplify merge
    
    * Use Object.assign in Blade generator too
    
    * Update test fixtures
    
    Co-authored-by: Jacob Baker-Kretzmar <jacobtbk@gmail.com>

--- tests/fixtures/admin.js
+++ tests/fixtures/admin.js
@@ -1,9 +1,7 @@
 const Ziggy = {"url":"http:\/\/ziggy.dev","port":null,"defaults":{},"routes":{"admin.dashboard":{"uri":"admin","methods":["GET","HEAD"]}}};
 
 if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
-    for (let name in window.Ziggy.routes) {
-        Ziggy.routes[name] = window.Ziggy.routes[name];
-    }
+    Object.assign(Ziggy.routes, window.Ziggy.routes);
 }
 
 export { Ziggy };
