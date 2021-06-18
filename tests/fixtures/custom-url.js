f3dfb2bdba895db82b758567e348f06a31cf6658
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-06-17 21:27:00 -0400

    Update test fixtures

--- tests/fixtures/custom-url.js
+++ tests/fixtures/custom-url.js
@@ -1,9 +1,7 @@
 const Ziggy = {"url":"http:\/\/example.org","port":null,"defaults":{"locale":"en"},"routes":{"postComments.index":{"uri":"posts\/{post}\/comments","methods":["GET","HEAD"]}}};
 
 if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
-    for (let name in window.Ziggy.routes) {
-        Ziggy.routes[name] = window.Ziggy.routes[name];
-    }
+    Object.assign(Ziggy.routes, window.Ziggy.routes);
 }
 
 export { Ziggy };
