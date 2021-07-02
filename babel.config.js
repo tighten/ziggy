b8ff643d0ffce62c77215e60471bdde350b747aa
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2021-07-02 02:09:23 -0400

    Revert "Fix babel config and transpile to es5, closes #440"
    This reverts commit d781b164b8f455730fe8a8b0cbf91f0f8cb55a73.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    presets: [
+        ['@babel/preset-env', { targets: 'defaults, not IE 11' }],
+    ],
     plugins: ['@babel/plugin-proposal-optional-chaining'],
     env: {
         test: {
