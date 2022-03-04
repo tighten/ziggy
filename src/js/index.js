5c4774391c56c52296788883687c0a2ed8d7d584
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-03-04 11:31:46 -0500

    Add `useRoute()` hook

--- src/js/index.js
+++ src/js/index.js
@@ -14,4 +14,12 @@ export const ZiggyVue = {
     }),
 };
 
+export const useRoute = (defaultConfig) => {
+    if (!defaultConfig && !globalThis.Ziggy && typeof Ziggy === 'undefined') {
+        throw new Error('Ziggy error: missing configuration. Ensure that a `Ziggy` variable is defined globally or pass a config object into the useRoute hook.');
+    }
+
+    return (name, params, absolute, config = defaultConfig) => route(name, params, absolute, config);
+};
+
 export default route;
