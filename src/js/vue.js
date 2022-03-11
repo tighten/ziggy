5661ec32937b7b84012a1d075f5a9c4d1954e9ce
Colin Fitz-Maurice <colin@fitz-maurice.com>
2022-03-11 12:48:46 -0500

    feat: Add Vue3 Provide/Inject Plugin (#518)
    * add Vue 3 provide/inject
    
    * add mixin back to vue 3 syntax
    
    * Remove global property for now
    
    * Fix
    
    Co-authored-by: Jacob Baker-Kretzmar <jacobtbk@gmail.com>

--- src/js/vue.js
+++ src/js/vue.js
@@ -1,9 +1,17 @@
 import route from './index.js';
 
 export const ZiggyVue = {
-    install: (v, options) => v.mixin({
-        methods: {
-            route: (name, params, absolute, config = options) => route(name, params, absolute, config),
-        },
-    }),
+    install: (v, options) => {
+        const r = (name, params, absolute, config = options) => route(name, params, absolute, config);
+
+        v.mixin({
+            methods: {
+                route: r,
+            },
+        });
+
+        if (parseInt(v.version) > 2) {
+            v.provide('route', r);
+        }
+    },
 };
