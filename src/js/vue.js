213323f0eca6a511e3dbf1c8756ae6ebe0d3a570
Colin <colin@fitz-maurice.com>
2022-03-10 23:29:56 -0500

    add Vue 3 provide/inject

--- src/js/vue.js
+++ src/js/vue.js
@@ -1,9 +1,11 @@
 import route from './index.js';
 
 export const ZiggyVue = {
-    install: (v, options) => v.mixin({
-        methods: {
-            route: (name, params, absolute, config = options) => route(name, params, absolute, config),
-        },
-    }),
+    install: (v, options) => {
+        const version = parseInt(v.version.split('.')[0]);
+        const _route = (name, params, absolute, config = options) => route(name, params, absolute, config);
+
+        v.mixin && version === 2 && (v.mixin({ methods: { route: _route } }));
+        v.provide && version > 2 && (v.provide('route', _route));
+    },
 };
