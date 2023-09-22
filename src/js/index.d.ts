3b0b74c81e2fcc5cfb0b004e222a46b8a81dbaf3
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2023-09-22 11:40:37 -0400

    Cleanup, add comments, remove unnecessary `string` unions, add basic `Router` type, fix `route()` exports

--- src/js/index.d.ts
+++ src/js/index.d.ts
@@ -1,13 +1,9 @@
-import Router from './Router';
-
 /**
  * A list of routes and their parameters and bindings.
  *
  * Extended and filled by the route list generated automatically by Ziggy.
  */
-export interface RouteList {
-    //
-}
+export interface RouteList {}
 
 /**
  * A route name registered with Ziggy.
@@ -18,6 +14,9 @@ type KnownRouteName = keyof RouteList;
  * A route name, or any string.
  */
 type RouteName = KnownRouteName | (string & {});
+// `(string & {})` prevents TypeScript from reducing this type to just `string`,
+// which would prevent intellisense from autocompleting known route names.
+// See https://stackoverflow.com/a/61048124/6484459.
 
 /**
  * Information about a single route parameter.
@@ -28,13 +27,14 @@ type ParameterInfo = { name: string, binding?: string };
  * A primitive route parameter value.
  */
 type ParameterValue = string | number;
+// TODO: Technically booleans work too, does it make sense to add them here? What would that look like?
 
 /**
- * A route parameter value, possibly nested inside an object under its binding key.
+ * A parseable route parameter, either as a plain value or nested inside an object under its binding key.
  */
 type ValueOrBoundValue<I extends ParameterInfo> = I extends { binding: string }
     ? { [K in I['binding']]: ParameterValue } | ParameterValue
-    : ParameterValue
+    : ParameterValue;
 // type A = ValueOrBoundValue<{ name: 'foo', binding: 'bar' }>;
 // = ParameterValue | { bar: ParameterValue }
 
@@ -43,26 +43,33 @@ type ValueOrBoundValue<I extends ParameterInfo> = I extends { binding: string }
  */
 type HasQueryParam = { _query?: Record<string, any> };
 /**
- * An object of parameters for an unknown route.
+ * An object of parameters for an unspecified route.
  */
 type GenericRouteParamsObject = Record<keyof any, any> & HasQueryParam;
+// `keyof any` essentially makes it function as a plain `Record`
 /**
- * An object of parameters for a specific named route registered with Ziggy.
+ * An object of parameters for a specific named route.
  */
-type KnownRouteParamsObject<I extends readonly ParameterInfo[]> = { [T in I[number] as T['name']]?: ValueOrBoundValue<T> | string } & HasQueryParam;
+type KnownRouteParamsObject<I extends readonly ParameterInfo[]> = { [T in I[number] as T['name']]?: ValueOrBoundValue<T> } & HasQueryParam;
+// `readonly` allows TypeScript to determine the actual values of all the
+// parameter names inside the array, instead of just seeing `string`.
+// See https://github.com/tighten/ziggy/pull/664#discussion_r1329978447.
 /**
  * An object of route parameters.
  */
 type RouteParamsObject<N extends RouteName> = N extends KnownRouteName ? KnownRouteParamsObject<RouteList[N]> : GenericRouteParamsObject;
 
 /**
- * An array of parameters for an unknown route.
+ * An array of parameters for an unspecified route.
  */
 type GenericRouteParamsArray = ValueOrBoundValue<ParameterInfo>[];
 /**
- * An array of parameters for a specific named route registered with Ziggy.
+ * An array of parameters for a specific named route.
  */
-type KnownRouteParamsArray<I extends readonly ParameterInfo[]> = { [K in keyof I]: ValueOrBoundValue<I[K]> | string };
+type KnownRouteParamsArray<I extends readonly ParameterInfo[]> = { [K in keyof I]: ValueOrBoundValue<I[K]> };
+// Because `K in keyof I` for a `readonly` array is always a number, even though
+// this looks like `{ 0: T, 1: U, 2: V }` TypeScript generates `[T, U, V]`.
+// See https://github.com/tighten/ziggy/pull/664#discussion_r1330002370.
 // type B = KnownRouteParamsArray<[{ name: 'post', binding: 'uuid' }]>;
 // = [ParameterValue | { uuid: ParameterValue }]
 /**
@@ -71,26 +78,32 @@ type KnownRouteParamsArray<I extends readonly ParameterInfo[]> = { [K in keyof I
 type RouteParamsArray<N extends RouteName> = N extends KnownRouteName ? KnownRouteParamsArray<RouteList[N]> : GenericRouteParamsArray;
 
 /**
- * All possible parameter arguments for a route.
+ * All possible parameter argument shapes for a route.
  */
 type RouteParams<N extends RouteName> = ParameterValue | RouteParamsObject<N> | RouteParamsArray<N>;
 
-type Config = {};
+interface Config {}
+
+type Router = {
+    current(): RouteName | undefined;
+    current<T extends RouteName>(name: T, params?: RouteParams<T>): boolean;
+    get params(): Record<string, any>;
+    has<T extends RouteName>(name: T): boolean;
+};
 
 /**
  * Ziggy's route helper.
  */
-// TODO 'default'
-export function route(): Router;
-export function route(
-    name: undefined,
-    params?: RouteParams<T> | undefined,
-    absolute?: boolean,
-    config?: Config,
-): Router;
-export function route<T extends RouteName>(
+export default function route(): Router;
+export default function route<T extends RouteName>(
     name: T,
     params?: RouteParams<T> | undefined,
     absolute?: boolean,
     config?: Config,
 ): string;
+export default function route(
+    name: undefined,
+    params: undefined,
+    absolute?: boolean,
+    config?: Config,
+): Router;
