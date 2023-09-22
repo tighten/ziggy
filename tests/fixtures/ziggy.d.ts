c4ce92a244a74dc46da1ec4154353b3531d14d44
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2023-09-22 16:21:07 -0400

    Recreate fixture files

---
+++ tests/fixtures/ziggy.d.ts
@@ -0,0 +1,12 @@
+/* This file is generated automatically! */
+declare module 'ziggy-js' {
+  interface RouteLookup {
+    "posts.index": [],
+    "postComments.store": [
+        {
+            "name": "post"
+        }
+    ]
+}
+}
+export {};
