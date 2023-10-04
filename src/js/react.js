2b5c84f7cb4a7853a25e4ac2ccca2f8d47ca3446
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2023-10-04 09:22:43 -0400

    Fix exports

---
+++ src/js/react.js
@@ -0,0 +1,9 @@
+import route from './index.js';
+
+export function useRoute(defaultConfig) {
+    if (!defaultConfig && !globalThis.Ziggy && typeof Ziggy === 'undefined') {
+        throw new Error('Ziggy error: missing configuration. Ensure that a `Ziggy` variable is defined globally or pass a config object into `useRoute()`.');
+    }
+
+    return (name, params, absolute, config = defaultConfig) => route(name, params, absolute, config);
+};
