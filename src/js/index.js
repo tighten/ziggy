5835480402c66e9fd4da1ce09c66934614e2f157
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2023-10-04 09:16:07 -0400

    Move `useRoute` to named export from root package

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,17 @@
 import Router from './Router';
 
-export default function route(name, params, absolute, config) {
+function route(name, params, absolute, config) {
     const router = new Router(name, params, absolute, config);
 
     return name ? router.toString() : router;
 }
+
+export function useRoute(defaultConfig) {
+    if (!defaultConfig && !globalThis.Ziggy && typeof Ziggy === 'undefined') {
+        throw new Error('Ziggy error: missing configuration. Ensure that a `Ziggy` variable is defined globally or pass a config object into `useRoute()`.');
+    }
+
+    return (name, params, absolute, config = defaultConfig) => route(name, params, absolute, config);
+};
+
+export default route;
