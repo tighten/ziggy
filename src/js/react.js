ffa9c60b8250b11e978f2c058c4a74af62d1fd76
Sai Gayathri Mantha <47597129+gayathrimantha@users.noreply.github.com>
2023-10-15 00:15:09 +0530

    Update react.js
    Added comments, instead of nesting the main logic inside an if statement, used early returns to make the code more readable and reduce nesting.

--- src/js/react.js
+++ src/js/react.js
@@ -1,12 +1,13 @@
 import route from './index.js';
 
 export function useRoute(defaultConfig) {
-    if (!defaultConfig && !globalThis.Ziggy && typeof Ziggy === 'undefined') {
-        throw new Error(
-            'Ziggy error: missing configuration. Ensure that a `Ziggy` variable is defined globally or pass a config object into `useRoute()`.',
-        );
+    // Check if defaultConfig is not provided and neither Ziggy nor globalThis.Ziggy exists
+    if (!defaultConfig && (typeof Ziggy === 'undefined' && !globalThis.Ziggy)) {
+        throw new Error('Ziggy error: missing configuration. Ensure that a `Ziggy` variable is defined globally or pass a config object into `useRoute()`.');
     }
 
+    // Return the route function with provided or default config
     return (name, params, absolute, config = defaultConfig) =>
         route(name, params, absolute, config);
 }
+
