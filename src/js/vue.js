aa596290b1500d5630d297bd22436e64a1443327
Guillaume hébert <guilheb@users.noreply.github.com>
2023-12-15 15:56:18 -0500

    Prise 3

--- src/js/vue.js
+++ src/js/vue.js
@@ -3,9 +3,10 @@ import { usePage } from '@inertiajs/vue3';
 
 export const ZiggyVue = {
     install(app, options) {
-        const r = (name, params, absolute, config = options) =>
+        const r = (name, params, absolute, config = options) => {
             config = config ?? usePage().props?.ziggy;
             route(name, params, absolute, config);
+        };
 
         app.mixin({
             methods: {
