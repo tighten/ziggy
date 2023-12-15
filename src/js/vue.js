c0e40c688826a9745dc317fa274eb08badd4ec6e
Guillaume hébert <guilheb@users.noreply.github.com>
2023-12-15 15:43:57 -0500

    Utiliser l'info de Ziggy à jour reçue par HandleInertiaRequest

--- src/js/vue.js
+++ src/js/vue.js
@@ -1,7 +1,10 @@
 import route from './index.js';
+import { usePage } from '@inertiajs/vue3';
 
 export const ZiggyVue = {
     install(app, options) {
+        options = options ?? usePage().props?.ziggy;
+
         const r = (name, params, absolute, config = options) =>
             route(name, params, absolute, config);
 
