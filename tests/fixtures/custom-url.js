3ce19702e9a1a965e7d780010bf9ef7448ee8426
Mohammed Helaiwa <eng.helewa@gmail.com>
2024-10-21 00:06:43 +0300

    Add graceful error handling to test fixtures

--- tests/fixtures/custom-url.js
+++ tests/fixtures/custom-url.js
@@ -1,4 +1,4 @@
-const Ziggy = {"url":"http:\/\/example.org","port":null,"defaults":{"locale":"en"},"routes":{"postComments.index":{"uri":"posts\/{post}\/comments","methods":["GET","HEAD"],"parameters":["post"]}}};
+const Ziggy = {"url":"http:\/\/example.org","port":null,"defaults":{"locale":"en"},"errors":{"graceful":false,"fallback_url":"#"},"routes":{"postComments.index":{"uri":"posts\/{post}\/comments","methods":["GET","HEAD"],"parameters":["post"]}}};
 if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
     Object.assign(Ziggy.routes, window.Ziggy.routes);
 }
