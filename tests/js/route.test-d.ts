c2c379be08a9eb35640a7ee72691bf5c7ed8c7ac
pataar <git@pataar.nl>
2026-03-05 16:08:27 +0100

    Brand RouteUrl type to distinguish it from plain strings
    RouteUrl was a plain alias for `string`, so there was no type-level
    distinction between a URL produced by `route()` and any arbitrary string.
    
    Now RouteUrl is a branded type: it's still assignable to `string`, but a
    plain `string` can't be assigned to `RouteUrl` without going through
    `route()`.

--- tests/js/route.test-d.ts
+++ tests/js/route.test-d.ts
@@ -112,6 +112,8 @@ assertType(route().current('posts.comments.show', 'foo'));
 assertType<string>(route('optional', { maybe: 'foo' }));
 assertType<string>(route('optional', 'foo'));
 assertType<RouteUrl>(route('posts.comments.show', 'foo'));
+// @ts-expect-error a plain string is not assignable to RouteUrl
+assertType<RouteUrl>('/posts/foo' as string);
 assertType<Router>(route(undefined, undefined, undefined, {} as Config));
 
 // Uncomment to test strict route name checking - invalid route names in this file should error
