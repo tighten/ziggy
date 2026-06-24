9c97f6613272b3bfdfc47a4f46bd415e811b22c6
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2026-06-24 09:41:58 -0400

    Restore index.esm.js in npm build

--- tsdown.npm.config.ts
+++ tsdown.npm.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'tsdown';
+import { writeFileSync } from 'node:fs';
 
 export default defineConfig({
     entry: './src/js/index.js',
     dts: false,
     platform: 'browser',
     minify: true,
+    hooks: {
+        'build:done'() {
+            writeFileSync('dist/index.esm.js', "export * from './index.js';\n");
+        },
+    },
 });
